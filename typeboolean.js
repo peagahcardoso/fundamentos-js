14ea9d2a83943d20f96724b8196b77daaf3b2d22
peagahcardoso <86807137+peagahcardoso@users.noreply.github.com>
2021-09-08 17:03:34 -0300

    Add files via upload

---
+++ typeboolean.js
@@ -0,0 +1,14 @@
+// Tipo boolean
+// true -> verdadeiro
+// false -> falso 
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = false;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
---
+++ typestring.js
@@ -0,0 +1,18 @@
+// tipo string
+
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = "senhaSuperSegura1234!"
+const StringDeNumero = "34567";
+const MeuNome = "Leonardo";
+const citacao = "Meu nome é ";
+
+// concatenação (+)
+
+console.log(citacao + MeuNome)
+
+
+//template string OU template literal
+
+
+
